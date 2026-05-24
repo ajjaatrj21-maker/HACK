46d7d1bae28578aa3db69dbcb5d7f1db191eb5a4
ajjaatrj21-maker <ajjaatrj21@gmail.com>
2026-05-24 11:14:19 +0530

    Update Soundcloud.py

--- ShrutiMusic/platforms/Soundcloud.py
+++ ShrutiMusic/platforms/Soundcloud.py
@@ -61,13 +61,13 @@ class SoundAPI:
         return track_details, xyz
 
 
-# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
+# ©️ Copyright Reserved - @GHOSTRIDERFIRE0 GHOST
 
 # ===========================================
-# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
+# ©️ ⚡ 𝙂𝙃𝙊𝙎𝙏᭄𝙑𝙄𝘽𝙀𝙎 🎧 (@GHOSTRIDERFIRE0)
 # 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
-# 📢 Telegram Channel : https://t.me/ShrutiBots
+# 📢 Telegram Channel : https://t.me/+lcTDbs8lbedlNTNl
 # ===========================================
 
 
-# ❤️ Love From ShrutiBots 
+# ❤️ Love From ⚡ 𝙂𝙃𝙊𝙎𝙏᭄𝙑𝙄𝘽𝙀𝙎 🎧
